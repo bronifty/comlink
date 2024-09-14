40a6348d6d4fae0352a63035c363e5e075d46b42
bronifty <bronifty@gmail.com>
2024-09-14 01:20:52 -0400

    this

--- docs/examples/01-simple-example/main.mjs
+++ docs/examples/01-simple-example/main.mjs
@@ -1,13 +1,28 @@
 import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
+import { observableInput1, observableInput2, observableTotal, observableTotalPlusOne } from './store.mjs';
 import { input1, input2, result, result2, result3 } from './DOMElements.mjs';
+
 
 const worker = new Worker("worker.mjs", { type: "module" });
 const obj = Comlink.wrap(worker);
 
 console.log('workerObj.mjs')
 console.log(await obj.observableInput1.value)
+
+observableTotal.subscribe((value) => {
+  result.textContent = value
+})
+
 input1.addEventListener('change', () => {
-  obj.observableInput1.value = input1.value
+  console.log('input1', input1.value)
+  obj.incInput1(input1.value)
+  console.log('obj.observableInput1', obj.observableInput1.value)
+})
+
+input2.addEventListener('change', () => {
+  console.log('input2', input2.value)
+  obj.incInput2(input2.value)
+  console.log('obj.observableInput2', obj.observableInput2.value)
 })
 
 // result.textContent = "Loading...";
